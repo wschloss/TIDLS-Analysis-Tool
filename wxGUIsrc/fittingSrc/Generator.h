fdaaf0f2640eb91f46a14d99484ab9734b1382f7
Walter Schlosser <wschloss@mymail.mines.edu>
2014-09-19 09:30:04 -0600

    Copied functionality code into gui source to begin modifing to work with the gui.  Updated makefile to compile all source code.  Make file must be located in a folder whose parent directory contains wx-config so that all gui headers are linked correctly

---
+++ wxGUIsrc/fittingSrc/Generator.h
@@ -0,0 +1,40 @@
+/*
+
+This class is responsible for generating simulated data
+as well as exporting it to a txt file for further analysis
+
+Author: Walter Schlosser
+
+*/
+
+#pragma once
+
+#include "EquationManager.h"
+#include <map>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Generator {
+public:
+	Generator(map<string,double> constantsMap);
+	void promptForDefectProperties();
+	void promptForRangeProperties();
+	void generateData();
+	void printDataToFile();
+
+private:
+	EquationManager* equations;
+	map<double,vector<double> > generatedData;
+	// Variables necessary to define a defect for data generation
+	double NA, Nt, Et, sigmaN, sigmaP;
+	// Define range of data generation
+	double minPowerN;
+	double maxPowerN;
+	double minTemp;
+	double maxTemp;
+	double tempStep;
+};
---
+++ wxGUIsrc/fittingSrc/Loader.h
@@ -0,0 +1,31 @@
+/*
+
+This class is responsible for loading model data from a
+txt file specified by the user.  Currently, model constants
+will be loaded from txt.  It may also be used to load and
+contain the data for analysis in the class Analyzer.
+
+Author: Walter Schlosser
+
+*/
+
+#pragma once
+
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+class Loader {
+public:
+	Loader();
+
+	void importConstantsFromFile(const string &fileName, map<string,double> &dataMap);
+	void importDataFromFile(const string &fileName, map<double,vector<double> > &dataMap);
+
+private:
+
+};
